288e6b451009e1910c48e96a68885dcffecce255
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-01-22 23:03:43 -0600

    Publishing folder ./docs/.vuepress/dist to gh-pages in 1/22/2021, 11:03:42 PM

---
+++ assets/js/8.75b88a8a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{729:function(t,e,n){"use strict";n.r(e);var o={methods:{show:function(){alert("Hello world!!!")}}},l=n(70),s=Object(l.a)(o,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("Bar",{on:{showAlert:this.show}})],1)}),[],!1,null,null,null);e.default=s.exports}}]);
