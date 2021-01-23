1a357ea0bfb57b8cf7569ee7a1d40c226a7d70e8
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-01-22 20:06:18 -0600

    Publishing folder ./docs/.vuepress/dist to gh-pages in 1/22/2021, 8:06:16 PM

---
+++ assets/js/5.6cc5a6d2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{744:function(t,a,e){"use strict";e.r(a);var n=e(70),s=Object(n.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("a-avatar",{attrs:{src:"https://avatars1.githubusercontent.com/u/36168284?s=460&u=342cf5276d96763483fda84e8731be9b5a694a10&v=4"}})}),[],!1,null,null,null);a.default=s.exports}}]);
